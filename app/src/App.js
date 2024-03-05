f97a538431a484363eeee9c51549149d5a777d8c
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-05 14:26:17 +0200

    feat: basic app structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,14 @@
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PageNotFound from './Pages/PageNotFound';
 import MainPage from './Pages/MainPage';
 
-const rollbarConfig = {
-  accessToken: 'a00fea07b869482b8ac0f66dac81ab98',
-  environment: 'testenv',
-};
-
 const App = () => (
-  <RollbarProvider config={rollbarConfig}>
-    <ErrorBoundary>
-      {/* <Provider store={store}> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-      {/* </Provider> */}
-    </ErrorBoundary>
-  </RollbarProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
