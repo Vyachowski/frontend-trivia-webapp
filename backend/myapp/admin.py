b0dedc050e348736d21e9af5abed1a52f0c06c9c
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-05 12:01:06 +0200

    feat: add to npm package and paste basic markup from previous project

--- backend/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-# from django.conf.urls import include
-# #and either, where the admin.py file exists
-# # import name_of_another_app.admin
-# # or
-# admin.autodiscover() 
-# urlpatterns = [
-# admin.register('admin/', include(admin.site.urls)),
-# ]
