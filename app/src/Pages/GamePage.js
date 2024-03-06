ed064ffd9fd3d361b9b27930b60bc26dd8334e3e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-06 15:25:38 +0200

    test: add error to check hooks

--- app/src/Pages/GamePage.js
+++ app/src/Pages/GamePage.js
@@ -4,17 +4,15 @@ import GameBoard from '../Components/GameBoard';
 import GamePanel from '../Components/GamePanel';
 import QuestionPanel from '../Components/QuestionPanel';
 
-const GamePage = () => {
-  return (
-    <Container>
-      <Header />
-      <div className='separator'/>
-      <GameBoard>
-        <GamePanel />
-        <QuestionPanel />
-      </GameBoard>
-    </Container>
-  );
-};
+const GamePage = () => (
+  <Container>
+    <Header />
+    <div className='separator' />
+    <GameBoard>
+      <GamePanel />
+      <QuestionPanel />
+    </GameBoard>
+  </Container>
+);
 
 export default GamePage;
