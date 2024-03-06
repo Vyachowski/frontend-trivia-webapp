b49b387555a52eedb6757582b183e6b5e36e625a
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-06 20:47:43 +0200

    feat: add object properties for progress bar

--- app/src/Components/ProgressBar.js
+++ app/src/Components/ProgressBar.js
@@ -17,21 +17,21 @@ const ProgressItem = styled.li`
 
 const ProgressBar = () => {
   const progress = [
-    { step: 15, name: 'Your first offer' },
-    { step: 14, name: '1 500 000' },
-    { step: 13, name: '800 000' },
-    { step: 12, name: '400 000' },
-    { step: 11, name: '200 000' },
-    { step: 10, name: 'Junior Dev' },
-    { step: 9, name: '50 000' },
-    { step: 8, name: '25 000' },
-    { step: 7, name: '15 000' },
-    { step: 6, name: '10 000' },
-    { step: 5, name: 'Web Dev' },
-    { step: 4, name: '3 000' },
-    { step: 3, name: '2 000' },
-    { step: 2, name: '1 000' },
-    { step: 1, name: '500' }
+    { step: 15, name: 'Your first offer', status: 'pending' },
+    { step: 14, name: '1 500 000', status: 'pending' },
+    { step: 13, name: '800 000', status: 'pending' },
+    { step: 12, name: '400 000', status: 'pending' },
+    { step: 11, name: '200 000', status: 'pending' },
+    { step: 10, name: 'Junior Dev', status: 'pending' },
+    { step: 9, name: '50 000', status: 'pending' },
+    { step: 8, name: '25 000', status: 'pending' },
+    { step: 7, name: '15 000', status: 'pending' },
+    { step: 6, name: '10 000', status: 'pending' },
+    { step: 5, name: 'Web Dev', status: 'pending' },
+    { step: 4, name: '3 000', status: 'pending' },
+    { step: 3, name: '2 000', status: 'pending' },
+    { step: 2, name: '1 000', status: 'pending' },
+    { step: 1, name: '500', status: 'active' }
   ];
 
   return (
