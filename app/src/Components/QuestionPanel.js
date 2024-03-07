26db1c75b672e97a6fc4a76bee2a6af0b5c4ace2
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-07 14:02:21 +0200

    feat: add markup for question panel

--- app/src/Components/QuestionPanel.js
+++ app/src/Components/QuestionPanel.js
@@ -1,5 +1,56 @@
+import styled from 'styled-components';
+
+const QuestionPanelWrapper = styled.section`
+  `;
+
+const Question = styled.div`
+
+  `;
+
+const Answers = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  `;
+
+const Answer = styled.li`
+  margin: 0;
+  padding: 0;
+  `;
+
+const AnswerButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  `;
+
 const QuestionPanel = () => (
-  <div>1</div>
+  <QuestionPanelWrapper>
+    <Question>
+      Do you know what is the answer?
+    </Question>
+    <Answers>
+      <Answer key={1}>
+        <AnswerButton>
+          Example of answer
+        </AnswerButton>
+      </Answer>
+      <Answer key={2}>
+        <AnswerButton>
+          Example of answer
+        </AnswerButton>
+      </Answer>
+      <Answer key={3}>
+        <AnswerButton>
+          Example of answer
+        </AnswerButton>
+      </Answer>
+      <Answer key={4}>
+        <AnswerButton>
+          Example of answer
+        </AnswerButton>
+      </Answer>
+    </Answers>
+  </QuestionPanelWrapper>
 );
 
 export default QuestionPanel;
