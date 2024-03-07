6462d842b69e0708cb6f097f5b06410a7f30c6e8
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-07 14:41:10 +0200

    feat: add new styles

--- app/src/Components/GameBoard.js
+++ app/src/Components/GameBoard.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 const Main = styled.main`
-  padding-block: 42px;
+  display: grid;
+  gap: 40px;
+  justify-content: center;
+  max-width: 1040px;
+  margin: 0 auto;
+  padding-top: 40px;
+  padding-inline: 20px;
 `;
 
 const GameBoard = ({ children }) => (
--- app/src/Components/GamePanel.js
+++ app/src/Components/GamePanel.js
@@ -5,8 +5,11 @@ import ProgressBar from './ProgressBar';
 
 const PanelWrapper = styled.div`
   display: flex;
-  flex-wrap: wrap;
   justify-content: space-between;
+  gap: 55px;
+  width: 95%;
+  max-height: 480px;
+  margin: 0 auto;
   `;
 
 const GamePanel = () => (
