fff07e21dab993fdb08686f935eac8325625b433
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-11 20:32:19 +0200

    feat: add css variable

---
+++ app/src/Components/cssVariables.js
@@ -0,0 +1,32 @@
+/* Main background colors */
+const mainBgColor = 'rgb(30, 38, 51)';
+const borderColor = 'rgb(58, 64, 78)';
+/* Text colors */
+const textColor = 'rgb(178, 186, 205)';
+const activeTextColor = 'rgb(52, 105, 195)';
+const accentColor = 'rgb(228, 144, 63)';
+/* Windows */
+const mainWindowsColor = 'rgb(35, 42, 58)';
+const windowActiveBackground = 'rgb(44, 52, 71)';
+const windowBorderRadius = '10px';
+const windowBorderColor = 'rgb(58, 64, 78)';
+const windowBorder = `2px solid ${windowBorderColor}`;
+const windowInnerBorderColor = 'rgb(47, 57, 76)';
+const windowInnerBorder = `2px solid ${windowInnerBorderColor}`;
+const windowShadowSmall = '0px 4px 4px 0px #00000040';
+
+export {
+  mainBgColor,
+  borderColor,
+  textColor,
+  activeTextColor,
+  accentColor,
+  mainWindowsColor,
+  windowActiveBackground,
+  windowBorderRadius,
+  windowBorderColor,
+  windowBorder,
+  windowInnerBorderColor,
+  windowInnerBorder,
+  windowShadowSmall,
+};
