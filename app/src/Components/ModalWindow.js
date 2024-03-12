150a50610e5fa9cf12bb6b3311583f9a5eef395f
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-12 20:53:29 +0200

    feat: add new component

---
+++ app/src/Components/ModalWindow.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  max-width: 364px;
+  border-radius: 10px 10px 0 0;
+  border: 2px solid #2F394C;
+  background: linear-gradient(180deg, #2A303E -3.25%, #161D2E 104.57%);
+`;
+
+const ModalWindow = () => (
+  <ModalWrapper>
+    1
+  </ModalWrapper>
+);
+
+export default ModalWindow;
