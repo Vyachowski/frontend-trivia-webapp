96006f89c8399a19055f1ace043c947a82182c59
Slava Haikin <vyachowski@gmail.com>
2024-07-27 13:44:48 +0700

    feat(header): add basic styles for header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,12 @@
 import Link from 'next/link';
 import styles from './Header.module.scss';
 import { SpeakerIcon } from './SpeakerIcon/SpeakerIcon';
-
-const ABOUT_LINK = '/about';
-const RULE_LINK = '/rules';
-const FEDBACK_LINK = '/feedback';
+import { ABOUT_LINK, RULE_LINK, FEEDBACK_LINK } from '@/src/constants';
 
 const NAV_CONFIG = {
   about: ABOUT_LINK,
   rules: RULE_LINK,
-  feedback: FEDBACK_LINK,
+  feedback: FEEDBACK_LINK,
 };
 
 export const Header = () => (
@@ -18,15 +15,22 @@ export const Header = () => (
       <nav className={styles.nav}>
         <ul className={styles.menu}>
           {Object.entries(NAV_CONFIG).map(([key, value]) => (
-            <li key={key}>
-              <Link href={value}>{key.toUpperCase()}</Link>
+            <li className={styles['menu-item']} key={key}>
+              <Link className={styles['menu-link']} href={value}>
+                {key.toUpperCase()}
+              </Link>
             </li>
           ))}
         </ul>
       </nav>
-      <button type="button">
-        <SpeakerIcon isSoundOff />
-      </button>
+      <div className={styles['settings-menu']}>
+        <button className={styles['lang-switcher']} type="button">
+          <span>EN</span>
+        </button>
+        <button className={styles['sound-toggle']} type="button">
+          <SpeakerIcon isSoundOff />
+        </button>
+      </div>
     </div>
   </header>
 );
