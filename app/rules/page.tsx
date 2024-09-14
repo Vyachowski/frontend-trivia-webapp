dd554d38595ae285e3f43f32e507184b4459cdcc
Slava Haikin <vyachowski@gmail.com>
2024-09-14 12:50:34 +0700

    feat: add rules page text

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -4,6 +4,12 @@ const cx = classNames.bind(styles);
 
 export default function AboutPage() {
   return (
-    <div className={cx('page-wrapper')}>Here is an info about the app!</div>
+    <p className={cx('page-wrapper')}>
+      A 15-step trivia game where players must answer challenging questions on
+      JavaScript, CSS, and HTML to reach the final stage. Test your coding
+      knowledge and climb to the top! You have three lifelines: 50/50 to remove
+      two incorrect options, &quot;Google It&quot; for a quick search, and AI
+      assistance for expert help.
+    </p>
   );
 }
